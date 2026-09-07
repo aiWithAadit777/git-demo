7a188706fc83d99ce24aa7944af29f828ce82a0b
Aadit Sharma Shiwakoti <vardanshiwakoti123@gmail.com>
2026-09-07 14:10:42 +0545

    Done Updates

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 # Git Demo
+<p>THis is a demo repo of implementing git from mine pc and publishing it in github.</p>
+<h1>Author Aadit Sharma Shiwakoti</h1>
--- model_training.py
+++ model_training.py
@@ -1,1 +1,10 @@
+import pandas as pd 
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import torch 
+import torch.nn as nn 
+import torch.optim as optim
 
+df=pd.read_csv('data.csv')
+df.head[5]
